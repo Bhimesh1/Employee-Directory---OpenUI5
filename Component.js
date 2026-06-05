5be46b4ba9b5b2de9e4f0899adb2984ab134cbe6
Bhimesh1 <bhimeshpatil1@gmail.com>
2026-06-05 14:58:43 +0200

    Add employee list view

--- Component.js
+++ Component.js
@@ -1,11 +1,21 @@
 sap.ui.define([
-  "sap/ui/core/UIComponent"
-], function (UIComponent) {
+  "sap/ui/core/UIComponent",
+  "sap/ui/model/json/JSONModel"
+], function (UIComponent, JSONModel) {
   "use strict";
 
   return UIComponent.extend("employee.directory.Component", {
     metadata: {
       manifest: "json"
+    },
+
+    init: function () {
+      UIComponent.prototype.init.apply(this, arguments);
+
+      const oEmployeeModel = new JSONModel();
+      oEmployeeModel.loadData("model/employees.json");
+
+      this.setModel(oEmployeeModel);
     }
   });
 });
