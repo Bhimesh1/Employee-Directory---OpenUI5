fcf62d661ac8d567bfb37125078f79f377a88652
Bhimesh1 <bhimeshpatil1@gmail.com>
2026-06-05 15:00:23 +0200

    Add employee search filter

--- controller/App.controller.js
+++ controller/App.controller.js
@@ -1,7 +1,33 @@
 sap.ui.define([
-  "sap/ui/core/mvc/Controller"
-], function (Controller) {
+  "sap/ui/core/mvc/Controller",
+  "sap/ui/model/Filter",
+  "sap/ui/model/FilterOperator"
+], function (Controller, Filter, FilterOperator) {
   "use strict";
 
-  return Controller.extend("employee.directory.controller.App", {});
+  return Controller.extend("employee.directory.controller.App", {
+    onSearch: function (oEvent) {
+      const sQuery = oEvent.getParameter("newValue");
+      const oList = this.byId("employeeList");
+      const oBinding = oList.getBinding("items");
+
+      if (!sQuery) {
+        oBinding.filter([]);
+        return;
+      }
+
+      const aFilters = [
+        new Filter("name", FilterOperator.Contains, sQuery),
+        new Filter("department", FilterOperator.Contains, sQuery),
+        new Filter("role", FilterOperator.Contains, sQuery)
+      ];
+
+      const oCombinedFilter = new Filter({
+        filters: aFilters,
+        and: false
+      });
+
+      oBinding.filter([oCombinedFilter]);
+    }
+  });
 });
